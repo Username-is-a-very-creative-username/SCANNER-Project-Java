d0166dd05785a7994d39450a88d44bdf8d07a173
Large Beard Dude <114947306+Username-is-a-very-creative-username@users.noreply.github.com>
2023-10-14 13:42:33 -0400

    Age-Detector-SCANNER.java

--- Age-Detector-SCANNER.java
+++ Age-Detector-SCANNER.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
+import java.util.Scanner; //Adds the scanner into the code 
 
 class Main {
   public static void main(String[] args) {
-    Scanner reader = new Scanner(System.in);
-    System.out.println("Enter your Age: ");
-    int age = reader.nextInt();
+    Scanner reader = new Scanner(System.in);//Makes a Scanner Class
+    System.out.println("Enter your Age: "); //Lets the Programmer know to enter their age
+    int age = reader.nextInt(); //makes the person's input to be viewed as their "Age" 
 
     if (age < 13) {
-      System.out.println("You are a child");
+      System.out.println("You are a child"); //If Age is smaller than "13" Call the programmer a child
     }
     if (age > 12 &&  age < 20) {
-      System.out.println("You are a Teen");
+      System.out.println("You are a Teen"); //If Age is older than "12" but younger than "20" Call the programmer a Teen
     }
     if (age > 19 && age < 65) { 
-      System.out.println("You are a adult");
+      System.out.println("You are a adult"); //If Age is older than "19" but younger than "65" Call the programmer an Adult
     }
     if (age > 64) {
-      System.out.println("You are an elder");
+      System.out.println("You are an elder"); //If Age is older than "64" Call the programmer an elder
     }
     
-    reader.close();
+    reader.close(); //Stop the scanner from reading anymore 
   }
 }
